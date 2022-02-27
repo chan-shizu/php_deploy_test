fed321a07cec9b819ba5d7ece37d790c9d599e2d
chan-shizu <bubbler018@gmail.com>
2022-02-28 01:27:19 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>HTML内でのPHP処理</title>
+</head>
+<body>
+<h1><?php echo 'タイトルです'; ?></h1>
+<?='HTML内でのPHPスクリプト実行処理'; ?>
+</body>
+</html>
